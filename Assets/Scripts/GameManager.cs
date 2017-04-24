f1ed5bd0b22481692628b4f1f24ffddf8f8c8a8f
Da_Macchi <damacchi91@gmail.com>
2017-04-24 16:35:53 +0100

    start
    inizio

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : MonoBehaviour  {
+    [Range(0.0f, 1.0f)]
+    public float topLimit;
+    [Range(0.0f, 1.0f)]
+    public float bottomLimit;
+    public GameObject genericNPC;
+
+    Vector2 topLimitVector;
+    Vector2 bottomLimitVector;
+
+    // Use this for initialization
+    void Start () {
+        topLimitVector = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, topLimit, 0f));
+        bottomLimitVector = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, bottomLimit, 0f));
+        for (int i = 0; i < 20; i++)
+        {
+            
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
---
+++ Assets/Scripts/NPCcontroller.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCcontroller : MonoBehaviour
+{
+
+    public Vector2 forwardDirection;
+    Vector2 realDirection;
+    public float speed;
+
+    Vector2 topLimit;
+    Vector2 bottomLimit;
+
+    public float timerLimit;
+    float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        realDirection = NewDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(realDirection * speed * Time.deltaTime);
+        if (GotToTheLimit())
+        {
+            realDirection = NewDirection();
+        }
+
+        timer += Time.deltaTime;
+        if (timer > timerLimit)
+        {
+            realDirection = NewDirection();
+            timer = 0f;
+        }
+
+    }
+
+    public Vector2 NewDirection()
+    {
+        float randomAngle;
+        if (transform.position.y > topLimit.y)
+        {
+            randomAngle = Random.Range(-45f, 0f);
+        }
+        else if (transform.position.y < bottomLimit.y)
+        {
+            randomAngle = Random.Range(0f, 45f);
+        }
+        else
+        {
+            randomAngle = Random.Range(-45f, 45f);
+        }
+        Vector2 direction = Quaternion.Euler(0f, 0f, randomAngle) * forwardDirection;
+        return direction;
+    }
+
+    public bool GotToTheLimit()
+    {
+        if (transform.position.y > topLimit.y || transform.position.y < bottomLimit.y)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void SetLimits(Vector2 top, Vector2 bottom)
+    {
+        topLimit = top;
+        bottomLimit = bottom;
+    }
+}
